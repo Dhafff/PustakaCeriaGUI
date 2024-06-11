bd249f05bb34a06f6c25b06153f4c284094e415c
Dhafff <dhafgoes@student.telkomuniversity.ac.id>
2024-06-11 20:24:58 +0700

    Menambahkan logic untuk form login dan register

--- PustakaCeriaGUI/FormLogin.cs
+++ PustakaCeriaGUI/FormLogin.cs
@@ -9,16 +9,63 @@ namespace PustakaCeriaGUI
         public FormLogin()
         {
             InitializeComponent();
+            txtPassword.PasswordChar = '*';
         }
 
         private void buttonMasuk_Click(object sender, EventArgs e) 
         { 
-            
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (ValidateLogin(username, password))
+            {
+                MessageBox.Show("Berhasil Masuk!");
+                this.Hide();
+                MainForm homePage = new MainForm();
+                homePage.Show();
+            }
+            else
+            {
+                MessageBox.Show("Nama pengguna atau kata sandi salah!");
+            }
         }
 
         private void buttonDaftar_Click(object sender, EventArgs e) 
         {
-            
+            FormRegister register = new FormRegister();
+            this.Hide();
+            register.ShowDialog();
+            this.Show();
+        }
+
+        private bool ValidateLogin(string username, string password)
+        {
+            if (File.Exists("users.json"))
+            {
+                var json = File.ReadAllText("users.json");
+                var users = JArray.Parse(json);
+
+                foreach (var user in users)
+                {
+                    if (user["username"].ToString() == username && VerifyPassword(password, user["password"].ToString()))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedHashedPassword)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var enteredPasswordBytes = Encoding.UTF8.GetBytes(enteredPassword);
+                var enteredPasswordHash = sha256.ComputeHash(enteredPasswordBytes);
+                var enteredPasswordHashString = Convert.ToBase64String(enteredPasswordHash);
+
+                return enteredPasswordHashString == storedHashedPassword;
+            }
         }
     }
 }
